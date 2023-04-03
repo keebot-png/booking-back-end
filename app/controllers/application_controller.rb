3abb841eae63348825a182c42386b547a762cbd6
keebot-png <kee.bot@icloud.com>
2023-04-03 18:40:56 +0200

    Add authentication endpoints.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+    before_action :update_allowed_parameters, if: :devise_controller?
+
+    protected
+
+    def update_allowed_parameters
+        devise_parameter_sanitizer.permit(:sign_up) do |u|
+        u.permit(:name)
+        end
+    end
 end
