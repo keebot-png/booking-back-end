4a8797d50e34525a5ef0cb73451a43627f7501e9
kennankole <kennankole@gmail.com>
2023-04-04 13:07:54 +0300

    Fixing linter errors

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -6,5 +6,4 @@ class Api::V1::CoursesController < ApplicationController
   end
 
   # POST /
-  
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,2 @@
 class Course < ApplicationRecord
 end
-# Added columns to the;
-#   1. courses tables
-#   2. reservations table 
-#   3. users table
-#   4.teachers table 
-# Added the relationships between the respective tables according to schema.
