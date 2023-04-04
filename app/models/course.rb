ca619cde753be1254745ca5977ff9f76d2625c78
kennankole <kennankole@gmail.com>
2023-04-04 13:02:51 +0300

    modified course.rb

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,8 @@
 class Course < ApplicationRecord
-  
 end
+# Added columns to the;
+#   1. courses tables
+#   2. reservations table 
+#   3. users table
+#   4.teachers table 
+# Added the relationships between the respective tables according to schema.
