b2f14caa0861555f6243c43232cc7aae0cdab11d
kennankole <kennankole@gmail.com>
2023-04-06 09:45:45 +0300

    Enabling CORS in application_controller for the API endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::API
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_cors_headers, :configure_permitted_parameters, if: :devise_controller?
   respond_to :json
+
+  private
+  def set_cors_headers
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allo-Methods'] = 'POST, GET, DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type'
+  end
 
   protected
 
