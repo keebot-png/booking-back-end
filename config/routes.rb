863ddc0e8fba5cbb8f1faa09b0a83ecea1810e5f
keebot-png <kee.bot@icloud.com>
2023-04-07 13:40:30 +0200

    Update routes and course controller.

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,9 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     namespace :v1 do
-      resources :courses do
-        resources :teachers 
-        resources :reservations
+      resources :courses, only: [:index, :show] do
+        resources :teachers, only: [:index, :show]
+        resources :reservations, only: [:index, :create, :destroy]
       end
     end
   end
