845c3fed7c1229aa838cd3323589c50c52da98c2
kennankole <kennankole@gmail.com>
2023-04-07 14:22:58 +0300

    Fixing linter errors

--- spec/requests/api/reservation_spec.rb
+++ spec/requests/api/reservation_spec.rb
@@ -1,26 +1,26 @@
 require 'swagger_helper'
-
+# rubocop:disable Metrics/BlockLength
 RSpec.describe 'api/v1/courses/{id}/reservations' do
-  path "/api/v1/courses/{id}/reservations" do
+  path '/api/v1/courses/{id}/reservations' do
     get 'Get all reservations' do
       tags 'Reservation'
       consumes 'application/json'
       response '200', 'reservation found' do
         schema type: :array,
-          properties: {
-            id: { type: :integer },
-            start_time: { type: :time },
-            end_time: { type: :time },
-            course_id: { type: :integer },
-            teacher_id: { type: :integer },
-            user_id: { type: :integer }
-          },
-          required: %w[id start_time end_time course_id teacher_id user_id]
-          run_test!
+               properties: {
+                 id: { type: :integer },
+                 start_time: { type: :time },
+                 end_time: { type: :time },
+                 course_id: { type: :integer },
+                 teacher_id: { type: :integer },
+                 user_id: { type: :integer }
+               },
+               required: %w[id start_time end_time course_id teacher_id user_id]
+        run_test!
       end
     end
   end
-  
+
   path '/api/v1/courses/{id}/reservations/' do
     post 'Creates Reservation' do
       tags 'Teacher'
@@ -39,9 +39,9 @@ RSpec.describe 'api/v1/courses/{id}/reservations' do
       }
       response '201', 'Reservation created successfully' do
         let(:reservation) do
-          { 
-            start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0, 0, Time.now.utc_offset) , 
-            end_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 0, 0, Time.now.utc_offset), 
+          {
+            start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0, 0, Time.now.utc_offset),
+            end_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 0, 0, Time.now.utc_offset),
             course_id: 1, teacher_id: 1,
             user_id: 1
           }
@@ -61,14 +61,14 @@ RSpec.describe 'api/v1/courses/{id}/reservations' do
       tags 'Reservation'
       produces 'application/json'
       response '200', 'reservation cancelled successfully' do
-        let(:id) {
+        let(:id) do
           Reservation.create(
-            start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0, 0, Time.now.utc_offset) , 
-            end_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 0, 0, Time.now.utc_offset), 
+            start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0, 0, Time.now.utc_offset),
+            end_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 0, 0, Time.now.utc_offset),
             course_id: 1, teacher_id: 1,
             user_id: 1
           ).id
-        }
+        end
         run_test!
       end
 
@@ -79,3 +79,4 @@ RSpec.describe 'api/v1/courses/{id}/reservations' do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
--- spec/requests/api/teachers_spec.rb
+++ spec/requests/api/teachers_spec.rb
@@ -1,29 +1,31 @@
 require 'swagger_helper'
 
+# rubocop:disable Metrics/BlockLength
 RSpec.describe 'api/teachers' do
-  path "/api/v1/courses/{id}/teachers" do
+  path '/api/v1/courses/{id}/teachers' do
     get 'Get all teachers' do
       tags 'Teachers'
       consumes 'application/json'
       response '200', 'courses found' do
         schema type: :array,
-          properties: {
-            id: { type: :integer },
-            name: { type: :string },
-            image: { type: :string },
-            years_of_experience: { type: :integer },
-            professional_summary: { type: :string },
-            hourly_rating: { type: :float },
-            start_time: { type: :time },
-            end_time: { type: :time },
-            course_id: { type: :integer }
-          },
-          required: %w[id name image years_of_experience professional_summary hourly_rating start_time end_time course_id]
-          run_test!
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 image: { type: :string },
+                 years_of_experience: { type: :integer },
+                 professional_summary: { type: :string },
+                 hourly_rating: { type: :float },
+                 start_time: { type: :time },
+                 end_time: { type: :time },
+                 course_id: { type: :integer }
+               },
+               required: %w[id name image years_of_experience professional_summary hourly_rating start_time end_time
+                            course_id]
+        run_test!
       end
     end
   end
-  
+
   path '/api/v1/courses/{id}/teachers/' do
     post 'Creates a teacher' do
       tags 'Teacher'
@@ -45,12 +47,12 @@ RSpec.describe 'api/teachers' do
       }
       response '201', 'Teacher created successfully' do
         let(:teacher) do
-          { 
-            name: 'Amos', image: 'http/img.png', 
+          {
+            name: 'Amos', image: 'http/img.png',
             years_of_experience: 10, professional_summary: 'PHD in maths',
-            hourly_rating: 50.50, 
-            start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0, 0, Time.now.utc_offset) , 
-            end_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 0, 0, Time.now.utc_offset), 
+            hourly_rating: 50.50,
+            start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0, 0, Time.now.utc_offset),
+            end_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 0, 0, Time.now.utc_offset),
             course_id: 1
           }
         end
@@ -65,36 +67,36 @@ RSpec.describe 'api/teachers' do
   end
 
   path '/api/v1/courses/{id}/teachers/{id}' do
-    # rubocop:enable Metrics/BlockLength
     parameter name: 'id', in: :path, type: :integer
     get 'Retrieves a teacher' do
       tags 'Teacher'
       produces 'application/json'
       response '200', 'teacher found' do
         schema type: :object,
-        properties: {
-          id: { type: :integer },
-          name: { type: :string },
-          image: { type: :string },
-          years_of_experience: { type: :integer },
-          professional_summary: { type: :string },
-          hourly_rating: { type: :float },
-          start_time: { type: :time },
-          end_time: { type: :time },
-          course_id: { type: :integer }
-        },
-        required: %w[id name image years_of_experience professional_summary hourly_rating start_time end_time course_id]
-        let(:id) {
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 image: { type: :string },
+                 years_of_experience: { type: :integer },
+                 professional_summary: { type: :string },
+                 hourly_rating: { type: :float },
+                 start_time: { type: :time },
+                 end_time: { type: :time },
+                 course_id: { type: :integer }
+               },
+               required: %w[id name image years_of_experience professional_summary hourly_rating start_time
+                            end_time course_id]
+        let(:id) do
           Teacher.create(
             name: 'Amos', image: 'http://school.com/math.jpg',
-            years_of_experience: 10, 
+            years_of_experience: 10,
             professional_summary: 'A PHD in maths',
             hourly_rating: 50.30,
-            start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0, 0, Time.now.utc_offset) , 
-            end_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 0, 0, Time.now.utc_offset), 
-            course_id: 1,
+            start_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 9, 0, 0, Time.now.utc_offset),
+            end_time: Time.new(Time.now.year, Time.now.month, Time.now.day, 10, 0, 0, Time.now.utc_offset),
+            course_id: 1
           ).id
-        }
+        end
         run_test!
       end
 
@@ -105,3 +107,4 @@ RSpec.describe 'api/teachers' do
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
